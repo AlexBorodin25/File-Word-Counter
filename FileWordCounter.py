75e22df29f8b08fa10efc4f7d6df9bc77b9a0dde
alexb <aborod1@wgu.edu>
2026-06-15 12:24:44 -0400

    added stopwords.txt and load_stopwords function

--- FileWordCounter.py
+++ FileWordCounter.py
@@ -1,13 +1,15 @@
 import string
 from collections import Counter
 
-def load_stopwords(filename="stopwords.txt")
+def load_stopwords(filename="stopwords.txt"):
     try:
         with open(filename, "r") as f:
             return {line.strip().lower() for line in f if line.strip()}
     except FileNotFoundError:
         print("Stopwords file not found")
         return set()
+
+STOPWORDS = load_stopwords()
 
 def read_file(filename):
     try:
